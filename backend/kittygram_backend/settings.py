622a54e5fd1dd9cdd0a5656663a29a42c1772c78
Амелия зарубина <ameliazr@mail.ru>
2025-09-12 13:25:12 +0300

    25.1

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv(
+SECRET_KEY = os.environ.get(
     'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y- q*uq1!4$-89$'
 )
 
@@ -18,7 +18,7 @@ ALLOWED_HOSTS = [
     '127.0.0.1'
 ]
 
-INSTALLED_APPS = [
+INSTALLED_APPS = os.getenv[
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
