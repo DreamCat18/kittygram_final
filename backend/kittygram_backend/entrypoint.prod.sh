3883803bede643d96332a882a1bda73c54ac3f51
Amelia <you@example.com>
2025-10-03 22:27:14 +0300

    /19

--- backend/kittygram_backend/entrypoint.prod.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Запуск миграций
-python manage.py migrate
-# Сборка статики
-python manage.py collectstatic --noinput
-# Копирование собранной статики
-cp -r /app/collected_static/. /backend_static/static/
-
-exec "$@"
