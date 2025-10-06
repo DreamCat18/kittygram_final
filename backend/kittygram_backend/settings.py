ffdaace90cbece21fbc050f3d0cdda3984b71ad9
Amelia <you@example.com>
2025-10-06 15:27:49 +0300

    25

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -106,7 +106,7 @@ STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / ' /var/www/kittygram/media'
-
+#/app/media/cats/images 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
